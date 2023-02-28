656ebede7647252dc774059b517ed1f8c5f598a2
Arthur <amarthur@usp.br>
2023-02-28 17:33:16 -0300

    Improve multiprocessing and code structure. Also allows creating csv for the database.

--- Scripts/create_database.py
+++ Scripts/create_database.py
@@ -1,4 +1,3 @@
-import itertools
 import time
 import urllib.request
 from multiprocessing import Pool
@@ -8,30 +7,34 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from directory_structure import DirectoryStructure
 from PIL import Image
-from tqdm import tqdm
 
 
 class Database:
 
-    def __init__(self, csv_file: str, database_name="Database", imgs_threshold=100):
+    def __init__(self, csv_file: str, database_name="Database", imgs_threshold=100, processes=64):
         # Parameters
         self.csv_file = csv_file
         self.database_name = database_name
         self.imgs_threshold = imgs_threshold
+        self.processes = processes
 
         # Directories
         self.dirs = DirectoryStructure(database_dir_name=database_name)
         self.csv_file_path = self.dirs.observations_dir / self.csv_file
 
-        # String Variables
+        # CSV String Variables
         self.id = "id"
         self.license = "license"
         self.img_url = "image_url"
-        self.image_name = "image_name"
+        self.img_name = "image_name"
+        self.img_path = "image_path"
         self.scientific_name = "scientific_name"
-        self.plot_name = "distribution.png"
 
-    def create_database(self, check_images=True, print_info=False, plot_graph=False):
+        # Naming Variables
+        self.plot_name = "distribution.png"
+        self.database_csv_name = Path(database_name + '.csv')
+
+    def create_database(self, check_images=True, create_csv=False, print_info=False, plot_graph=False):
         # Info dict
         info = {}
 
@@ -51,22 +54,28 @@ class Database:
         df = df.groupby(self.scientific_name).filter(lambda x: len(x) >= self.imgs_threshold)
         info["Nº imgs after removing species below threshold"] = len(df)
 
-        # Create the database
+        # Create directories
         species_index = sorted(df[self.scientific_name].unique())
-        species_groups = df.groupby(self.scientific_name)
         info["Nº classes"] = len(species_index)
 
         print("Creating directories...")
         self.create_species_directories(species_index)
         print("Finished creating directories.\n")
 
+        # Download images
+        species_groups = df.groupby(self.scientific_name)
+        groups_df = [self.define_images(species_group) for _, species_group in species_groups]
+        images_df = pd.concat(groups_df, axis=0)
+
         print("Downloading images...")
-        for _, species_group in species_groups:
-            self.save_images(species_group)
+        self.save_images(df=images_df)
         print("Finished downloading images.\n")
 
         if check_images:
-            self.check_images()
+            self.check_images(df=images_df)
+
+        if create_csv:
+            self.create_csv(df=images_df)
 
         if print_info:
             self.print_info(info)
@@ -79,32 +88,38 @@ class Database:
             dir_location = self.dirs.database_dir / species_name
             self.create_directory(dir_location)
 
-    def save_images(self, species_group):
-        species_group = species_group.reset_index()
-        i = species_group.index.astype(str)
-        urls = species_group[self.img_url]
-        ext = urls.apply(lambda x: Path(x).suffix)
+    def define_images(self, species_group):
+        # Get group data
+        group_data = species_group.reset_index()
+        i = group_data.index.astype(str)
+        ext = group_data[self.img_url].apply(lambda x: Path(x).suffix)
 
         # Naming
-        species_name = species_group[self.scientific_name][0]
+        species_name = group_data[self.scientific_name][0]
         image_name = species_name.replace(" ", "_")
 
         # Define image names and paths
-        species_group[self.image_name] = image_name + "_" + i + ext
-        paths = species_group[self.image_name].apply(lambda x: self.dirs.database_dir / species_name / x)
-        func_args = [(url, path) for url, path in zip(urls, paths)]
+        group_data[self.img_name] = image_name + "_" + i + ext
+        group_data[self.img_path] = group_data[self.img_name].apply(lambda x: self.dirs.database_dir / species_name / x)
+        return group_data
 
-        # Multiprocess
-        processes = 64
-        with Pool(processes) as pool:
-            pool.starmap_async(self.save_img_from_url, func_args)
+    def save_images(self, df):
+        urls_paths = [(url, path) for url, path in zip(df[self.img_url], df[self.img_path])]
+        self.multiprocess(self.download_image, urls_paths)
+
+    def check_images(self, df):
+        paths = [path for path in df[self.img_path]]
+        self.multiprocess(self.check_image, paths)
+
+    def multiprocess(self, func, func_args):
+        with Pool(processes=self.processes) as pool:
+            pool.starmap_async(func, func_args)
             pool.close()
             pool.join()
 
-    def check_images(self):
-        images = list(itertools.chain.from_iterable(self.dirs.database_dict().values()))
-        for img in tqdm(images, desc="Checking images", unit=" images"):
-            self.check_image(img)
+    def create_csv(self, df):
+        header = [self.id, self.license, self.img_name, self.scientific_name, self.img_url]
+        df.to_csv(self.dirs.database_dir / self.database_csv_name, columns=header, index=False)
 
     def plot_distribution_graph(self, df):
         species_distribution = df[self.scientific_name].value_counts().sort_values(ascending=False)
@@ -129,7 +144,7 @@ class Database:
         Path(dir_location).mkdir(parents=True, exist_ok=True)
 
     @staticmethod
-    def save_img_from_url(url, img_path):
+    def download_image(url, img_path):
         if img_path.exists():
             return
 
@@ -150,13 +165,11 @@ class Database:
                 img.verify()
         except Exception as e:
             print(f"Image '{image}': {e}")
-            print(f"Removed '{image}'")
-            Path.unlink(image)
 
 
 def main():
     db = Database(csv_file="spiders.csv", imgs_threshold=100)
-    db.create_database(check_images=True, print_info=True, plot_graph=False)
+    db.create_database(check_images=True, create_csv=True, print_info=True, plot_graph=False)
 
 
 if __name__ == "__main__":
