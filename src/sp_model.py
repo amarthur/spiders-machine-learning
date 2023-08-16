8a7d977b83ebf14bd6fe06d78824391f74265b80
Arthur <amarthur@usp.br>
2023-08-15 22:32:06 -0300

    Update get model method

--- src/sp_model.py
+++ src/sp_model.py
@@ -16,55 +16,58 @@ class SpModel(pl.LightningModule):
         super().__init__()
         self.num_classes = num_classes
         self.criterion = criterion
-        self.feature_extractor, self.classifier = self.get_model()
+        self.model = self.get_model()
 
-        self.train_acc = MulticlassAccuracy(self.num_classes)
-        self.train_precision = MulticlassPrecision(self.num_classes)
-        self.train_recall = MulticlassRecall(self.num_classes)
-        self.train_f1 = MulticlassF1Score(self.num_classes)
+        self.train_acc = MulticlassAccuracy(self.num_classes, average="macro")
+        self.train_acc_micro = MulticlassAccuracy(self.num_classes, average="micro")
+        self.train_precision = MulticlassPrecision(self.num_classes, average="macro")
+        self.train_recall = MulticlassRecall(self.num_classes, average="macro")
+        self.train_f1 = MulticlassF1Score(self.num_classes, average="macro")
         self.train_conf_mat = MulticlassConfusionMatrix(self.num_classes)
 
-        self.val_acc = MulticlassAccuracy(self.num_classes)
-        self.val_precision = MulticlassPrecision(self.num_classes)
-        self.val_recall = MulticlassRecall(self.num_classes)
-        self.val_f1 = MulticlassF1Score(self.num_classes)
+        self.val_acc = MulticlassAccuracy(self.num_classes, average="macro")
+        self.val_acc_micro = MulticlassAccuracy(self.num_classes, average="micro")
+        self.val_precision = MulticlassPrecision(self.num_classes, average="macro")
+        self.val_recall = MulticlassRecall(self.num_classes, average="macro")
+        self.val_f1 = MulticlassF1Score(self.num_classes, average="macro")
         self.val_conf_mat = MulticlassConfusionMatrix(self.num_classes)
 
         self.train_metrics = {
-            "Train Accuracy": self.train_acc,
+            "Train Macro Accuracy": self.train_acc,
+            "Train Micro Accuracy": self.train_acc_micro,
             "Train Precision": self.train_precision,
             "Train Recall": self.train_recall,
             "Train F1": self.train_f1,
         }
 
         self.val_metrics = {
-            "Val Accuracy": self.val_acc,
+            "Val Macro Accuracy": self.val_acc,
+            "Val Micro Accuracy": self.val_acc_micro,
             "Val Precision": self.val_precision,
             "Val Recall": self.val_recall,
             "Val F1": self.val_f1,
         }
 
     def get_model(self):
-        backbone = models.resnet50(weights="DEFAULT")
-        num_filters = backbone.fc.in_features
-        layers = list(backbone.children())[:-1]
+        pretrained_model = models.resnet50(weights="DEFAULT")
 
-        feature_extractor = nn.Sequential(*layers)
+        num_filters = pretrained_model.fc.in_features
         classifier = nn.Sequential(
             nn.Linear(num_filters, 256),
             nn.ReLU(),
             nn.Dropout(0.4),
             nn.Linear(256, self.num_classes),
         )
+        pretrained_model.fc = classifier
 
-        return feature_extractor, classifier
+        for name, param in pretrained_model.named_parameters():
+            if "layer4" not in name:
+                param.requires_grad = False
+
+        return pretrained_model
 
     def forward(self, x):
-        self.feature_extractor.eval()
-        with torch.no_grad():
-            representations = self.feature_extractor(x).flatten(1)
-        x = self.classifier(representations)
-        return x
+        return self.model(x)
 
     def training_step(self, batch, batch_idx):
         x, y = batch
