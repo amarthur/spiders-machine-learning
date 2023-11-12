b7d24baef1f98e562d0c4e2714ad940a5c69362a
Arthur <amarthur@usp.br>
2023-11-12 18:49:20 -0300

    feat: Improve transformations

--- src/sp_transform.py
+++ src/sp_transform.py
@@ -5,17 +5,12 @@ class SpDataTransforms:
     def __init__(self) -> None:
         self._train_transforms = transforms.Compose(
             [
-                transforms.RandomResizedCrop(size=256, scale=(0.8, 1.0)),
-                transforms.RandomRotation(degrees=15),
-                transforms.RandomHorizontalFlip(p=0.5),
-                transforms.RandomVerticalFlip(p=0.5),
-                transforms.CenterCrop(size=224),
-                transforms.ColorJitter(
-                    brightness=0.2,
-                    contrast=0.2,
-                    saturation=0.2,
-                    hue=0.1,
-                ),
+                transforms.RandomResizedCrop(size=256, scale=(0.8, 1.0)), # zoom
+                transforms.RandomRotation(degrees=15), # rotation
+                transforms.RandomHorizontalFlip(p=0.5), # flip
+                transforms.RandomVerticalFlip(p=0.5), # flip
+                transforms.ColorJitter(brightness=0.2), # brightness
+                transforms.CenterCrop(size=224), # crop
                 transforms.ToTensor(),
                 transforms.Normalize(
                     mean=[0.485, 0.456, 0.406],
@@ -35,6 +30,18 @@ class SpDataTransforms:
                 ),
             ]
         )
+        
+        self._test_transforms = transforms.Compose(
+            [
+                transforms.Resize(size=256),
+                transforms.CenterCrop(size=224),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406],
+                    std=[0.229, 0.224, 0.225],
+                ),
+            ]
+        )
 
     @property
     def train_transforms(self):
@@ -43,3 +50,7 @@ class SpDataTransforms:
     @property
     def val_transforms(self):
         return self._val_transforms
+
+    @property
+    def test_transforms(self):
+        return self._test_transforms
