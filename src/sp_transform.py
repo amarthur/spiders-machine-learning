9125ad81cc31fa7baa8d1f2b704dc280d1db06da
Arthur <amarthur@usp.br>
2023-11-13 23:04:42 -0300

    refactor: Share common variables

--- src/sp_transform.py
+++ src/sp_transform.py
@@ -3,42 +3,48 @@ from torchvision import transforms
 
 class SpDataTransforms:
     def __init__(self) -> None:
+        self.resize = 256
+        self.center_crop = 224
+
+        self.mean = [0.485, 0.456, 0.406]
+        self.std = [0.229, 0.224, 0.225]
+
         self._train_transforms = transforms.Compose(
             [
-                transforms.RandomResizedCrop(size=256, scale=(0.8, 1.0)), # zoom
+                transforms.RandomResizedCrop(size=self.resize, scale=(0.8, 1.0)), # zoom
                 transforms.RandomRotation(degrees=15), # rotation
                 transforms.RandomHorizontalFlip(p=0.5), # flip
                 transforms.RandomVerticalFlip(p=0.5), # flip
                 transforms.ColorJitter(brightness=0.2), # brightness
-                transforms.CenterCrop(size=224), # crop
+                transforms.CenterCrop(size=self.center_crop), # crop
                 transforms.ToTensor(),
                 transforms.Normalize(
-                    mean=[0.485, 0.456, 0.406],
-                    std=[0.229, 0.224, 0.225],
+                    mean=self.mean,
+                    std=self.std,
                 ),
             ]
         )
 
         self._val_transforms = transforms.Compose(
             [
-                transforms.Resize(size=256),
-                transforms.CenterCrop(size=224),
+                transforms.Resize(size=self.resize),
+                transforms.CenterCrop(size=self.center_crop),
                 transforms.ToTensor(),
                 transforms.Normalize(
-                    mean=[0.485, 0.456, 0.406],
-                    std=[0.229, 0.224, 0.225],
+                    mean=self.mean,
+                    std=self.std,
                 ),
             ]
         )
-        
+
         self._test_transforms = transforms.Compose(
             [
-                transforms.Resize(size=256),
-                transforms.CenterCrop(size=224),
+                transforms.Resize(size=self.resize),
+                transforms.CenterCrop(size=self.center_crop),
                 transforms.ToTensor(),
                 transforms.Normalize(
-                    mean=[0.485, 0.456, 0.406],
-                    std=[0.229, 0.224, 0.225],
+                    mean=self.mean,
+                    std=self.std,
                 ),
             ]
         )
